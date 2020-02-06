e51da4a2612391a85820c272f41903dfc0a03fcd
Arvin Lleva <arvin.lleva@yahoo.com>
2020-02-06 13:26:10 -0800

    We implemented the input property with the get/set block

--- src/app/customers/customers-list/customers-list.component.ts
+++ src/app/customers/customers-list/customers-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import { ICustomer } from '../../shared/interfaces'
 
@@ -7,6 +7,23 @@ import { ICustomer } from '../../shared/interfaces'
   templateUrl: './customers-list.component.html'
 })
 export class CustomersListComponent implements OnInit {
+  // @Input() customers: any[]
+  // this is basically a private variable that is a type ICustomer from the interface
+  
+  private _customers: ICustomer[] = [];
+  
+  // the use of getters and setters
+  // customers of type ICustomer which is an array
+  @Input() get customers(): ICustomer[] {
+    return this._customers;
+  }
+
+  set customers(Value: ICustomer[]) {
+    if (value) {
+      this.fileredCustomers = this._custeroms = value;
+      this.calculateOrders
+    }
+  }
 
   filteredCustomers: ICustomer[] = []; // this will be an empty array from the start
   customersOrderTotal: number; // we'll get this dynamicall
@@ -17,6 +34,11 @@ export class CustomersListComponent implements OnInit {
   ngOnInit() {
 
   }
+
+  /*ngOnChanges(changes: SimpleChanges) {
+
+  }
+  */
 
   calculateOrders() {
     this.customersOrderTotal = 0;
